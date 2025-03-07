a99df005120e62fe5745710b40abc4aea329a11c
Christian Julian Jimenez <zs22016079@estudiantes.uv.mx>
2025-03-07 10:41:08 -0600

    Rutas flask

--- flask01/app.py
+++ flask01/app.py
@@ -2,9 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # http://
+def hola():
+    return "Hola Mundo"
+
+def adios():
+    return "Adiós Mundo"
+
+@app.route('/')
+def mensaje():
+    return f"{hola()} {adios()}"
+
+@app.route('/hola')
 def hola_mundo():
-    return 'Hola Mundo'
+    return '<h1 style="color: red;">Hola Mundo</h1>'
+
+@app.route('/json')
+def json():
+    return '{"nombre": "Christian"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1.0"?><nombre>Christian</nombre>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
+    
